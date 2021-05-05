45087388c278045ff206dc4a5524df305e3fb570
YesidF <dorianyesid09@hotmail.com>
2021-05-04 19:10:57 -0500

    Update NotificacionesIPN.php

--- NotificacionesIPN.php
+++ NotificacionesIPN.php
@@ -2,13 +2,13 @@
 
     // SDK de Mercado Pago
     require __DIR__ .  '/vendor/autoload.php';
-    /*// Agrega credenciales
+    // Agrega credenciales
     MercadoPago\SDK::setAccessToken('APP_USR-2572771298846850-120119-a50dbddca35ac9b7e15118d47b111b5a-681067803');
     MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
     $merchant_order = null;
 
-    switch($_GET["topic"]) {
+    /*switch($_GET["topic"]) {
         case "payment":
             $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
             // Get the payment and the corresponding merchant_order reported by the IPN.
