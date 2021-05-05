60b04d02a6995da33e3950132933d18fb23cb7cf
YesidF <dorianyesid09@hotmail.com>
2021-05-05 09:50:48 -0500

    Update success.php

--- success.php
+++ success.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> 
     </head>
+&collection_status=approved&payment_id=14734201818&status=approved&external_reference=dorianyesid09@hotmail.com&payment_type=account_money&merchant_order_id=2633921333&preference_id=681067803-cfb4510e-b8e2-4118-b9d6-51c06045cc1c&site_id=MCO&processing_mode=aggregator&merchant_account_id=null
     <body>
         <div class="container">
             <div class="row">
@@ -12,17 +13,17 @@
                     <h4 class="alert-heading">Información del pago</h4>
                     <p>La compra ha finalizado correctamente,Hemos recibido tu pago, el producto será despachado en brevedad y serás notificado por correo electrónico.</p>
                     <hr>
-                    <h3 class="alert-heading">Información Query String</h3>
+                    <h6 class="alert-heading">Información Query String</h6>
                     <p class="mb-0">
-                        collection_id=[PAYMENT_ID]
-                        collection_status=approved
+                        <
+                    </p><span class="">collection_id:</span><?php $_GET[collection_id] ?  $_GET[collection_id]: "No disponible" ?></p>
+                    collection_status=approved
                         external_reference=[EXTERNAL_REFERENCE]
                         payment_type=credit_card
                         preference_id=[PREFERENCE_ID]
                         site_id=[SITE_ID]
                         processing_mode=aggregator
                         merchant_account_id=null
-                    </p>
                 </div>
             </div>
         </div>
