24f587e024c7870c859b789fa8dda1d692432065
YesidF <dorianyesid09@hotmail.com>
2021-05-06 16:53:34 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -78,7 +78,7 @@
         </script>
     </head>
     <body>
-        <img src="./assets/music-audio-alp-201709" class="img-fluid" alt="Imagen Tienda e-comerce">
+        <img src="./assets/music-audio-alp-201709" class="img-fluid" max-width="100%" height¨="auto" alt="Imagen Tienda e-comerce">
         <div class="container">
             
         </div>
